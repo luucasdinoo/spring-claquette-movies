b46de1cd1abc6916d3444217cd63b6c8a441e56f
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-11 16:58:31 -0300

    Initial commit

---
+++ src/main/java/com/dev/dino/SpringClaquetteMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClaquetteMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClaquetteMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/dino/SpringClaquetteMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClaquetteMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
